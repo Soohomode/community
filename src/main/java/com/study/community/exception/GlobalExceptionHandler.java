93547264fe1e5b1298e61c84a6f3a22ff0bbc48e
soohomode <soohomode@gmail.com>
2026-05-03 21:33:20 +0900

    feat: 전역 예외 처리 추가 (GlobalExceptionHandler)

---
+++ src/main/java/com/study/community/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.study.community.exception;
+
+import com.study.community.common.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // EntityNotFoundException 처리 (PostNotFoundException 등 포함)
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.fail(e.getMessage()));
+    }
+
+    // 그 외 모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.fail("서버 오류가 발생했습니다."));
+    }
+}
