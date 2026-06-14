d03ad13dd3bc5f3364e8d442878cba6454d18c00
soohomode <soohomode@gmail.com>
2026-06-15 00:25:14 +0900

    feat: 프로덕션 환경 API URL 설정

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080' // 백엔드 API의 기본 URL (한곳에서 관리)
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
 });
 
 // 요청마다 토큰 자동 추가
